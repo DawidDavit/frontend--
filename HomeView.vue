d22f2579ffb95f8afffbed3bdaf12b08a6364b58
DawidDavit <wpmedamidawid177@gmail.com>
2024-10-11 12:50:32 +0200

    Create HomeView.vue

---
+++ HomeView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import axios from "axios";
+
+export default {
+  name: "HomeView",
+  data(){
+    return {
+      message: "",
+    };
+  },
+}
+
+
+async function sendMessage() {
+  await axios.post('http://localhost:3200/test', {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: this.message }),
+  });
+  const results = await response.json();
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="message" placeholder="Type your message" />
+    <button @click="sendMessage">Send</button>
+  </div>
+</template>
